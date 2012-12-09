8f8ea9dd3e239d8eaa39a33add2acfcb9c88d560
imuw <wil.linebaugh@gmail.com>
2012-12-09 10:40:29 -0600

    moving along

---
+++ vim.py
@@ -0,0 +1,13 @@
+
+from ninja_ide.core import plugin
+
+
+
+
+class Vim(plugin.Plugin):
+    def initialize(self):
+        pass
+    def finish(self):
+        pass
+    def get_prefrences_widget(self):
+        pass
